2040c8d455f6815a8c330b1881822ebac214f64c
alokectron <alokm@ectron.com>
2023-10-18 17:38:02 +0530

    async wrap

--- index.ts
+++ index.ts
@@ -10,14 +10,20 @@ const client = new Client({
   password: "",
   port: 5432,
 });
-client.connect(function (err: any) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-client.query(`CREATE DATABASE IF NOT EXISTS PSS(id bigint,email varchar(500), question varchar(300), answer varchar(300), query varchar(300));`)
+
+(async () => {
+  client.connect(function (err: any) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+  await client.query(
+    `CREATE DATABASE IF NOT EXISTS PSS(id bigint, email varchar(500), question varchar(300), answer varchar(300), query varchar(300));`
+  );
+})();
+
 const corsOptions = {
   origin: "*",
-  //  credentials:true,            //access-control-allow-credentials:true
+  credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 
@@ -39,9 +45,11 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.post("/form-data", (req: Request, res: Response) => {
-  console.log(req.query);
+app.post("/form-data", async (req: Request, res: Response) => {
   console.log(req.body);
+  for (const key in req.body) {
+    // await client.query(`INSERT INTO PSS () VALUES (${req.body[key]})`);
+  }
   res.send("success");
 });
 
