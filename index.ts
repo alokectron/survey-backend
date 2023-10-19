c3642ed59036a00add31b111b0fbda7b697b6a11
Alok Mishra <>
2023-10-20 04:40:44 +0530

    get api added with post optimisation

--- index.ts
+++ index.ts
@@ -4,20 +4,19 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import { Client } from "pg";
 const client = new Client({
-  user: "",
-  host: "",
-  database: "",
+  user: "postgres",
+  host: "localhost",
+  database: "alokmishra",
   password: "",
-  port: 5432,
+  port: 5433,
 });
 
 (async () => {
   client.connect(function (err: any) {
     if (err) throw err;
-    console.log("Connected!");
   });
   await client.query(
-    `CREATE DATABASE IF NOT EXISTS PSS(id bigint, email varchar(500), question varchar(300), answer varchar(300), query varchar(300));`
+    `CREATE TABLE IF NOT EXISTS PSS(id serial not null primary key, email varchar(500), question varchar(300), answer varchar(300));`
   );
 })();
 
@@ -47,10 +46,43 @@ app.get("/", (req: Request, res: Response) => {
 
 app.post("/form-data", async (req: Request, res: Response) => {
   console.log(req.body);
+  const email = req.body.email;
+  const { rows } = await client.query(
+    `SELECT * FROM PSS WHERE email = '${email}'`
+  );
+  delete req.body.email;
   for (const key in req.body) {
-    // await client.query(`INSERT INTO PSS () VALUES (${req.body[key]})`);
+    console.log(rows.length);
+    if (rows.length == 0) {
+      console.log("abc");
+      await client.query(
+        `INSERT INTO PSS (email, question, answer) VALUES ('${email}','${key}','${req.body[key]}');`
+      );
+    } else {
+      await client.query(
+        `UPDATE PSS SET answer='${req.body[key]}' where email = '${email}' and question='${key}';`
+      );
+    }
   }
   res.send("success");
+});
+
+app.get("/form-data", async (req: Request, res: Response) => {
+  let rows;
+  ({ rows } = await client.query(`SELECT * FROM PSS`));
+  if (req.query.email)
+    ({ rows } = await client.query(
+      `SELECT * FROM PSS WHERE email = '${req.query.email}'`
+    ));
+  if (req.query.question)
+    ({ rows } = await client.query(
+      `SELECT * FROM PSS WHERE question = '${req.query.question}'`
+    ));
+  if (req.query.question && req.query.answer)
+    ({ rows } = await client.query(
+      `SELECT * FROM PSS WHERE question = '${req.query.question}' and answer = '${req.query.answer}'`
+    ));
+  res.send(rows);
 });
 
 app.listen(port, () => {
