a61f9727f6667f53e5e8674f958d284d1ec5ea19
alokectron <alokm@ectron.com>
2023-11-20 18:03:32 +0530

    postgres to azureSQL migration

--- index.ts
+++ index.ts
@@ -11,13 +11,53 @@ const client = new Client({
   password: "Mafa@3874",
   port: 5432,
 });
+const sql = require("mssql");
+
+const config = {
+  user: "master", // better stored in an app setting such as process.env.DB_USER
+  password: "L7_qsNmF-2gj_Vn", // better stored in an app setting such as process.env.DB_PASSWORD
+  server: "smarteye-surveys.database.windows.net", // better stored in an app setting such as process.env.DB_SERVER
+  port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
+  database: "mrkt-srvys-db", // better stored in an app setting such as process.env.DB_NAME
+  authentication: {
+    type: "default",
+  },
+  options: {
+    encrypt: true,
+  },
+};
+
+connectAndQuery();
+async function connectAndQuery() {
+  try {
+    let poolConnection = await sql.connect(config);
+    await poolConnection
+      .request()
+      .query(
+        `CREATE TABLE packaging_station(id INT NOT NULL IDENTITY(1, 1), name varchar(500), email varchar(500), question varchar(300), answer varchar(300),date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL);`
+      );
+    await poolConnection
+      .request()
+      .query(
+        `CREATE TABLE users(id INT NOT NULL IDENTITY(1, 1), username varchar(500), password varchar(300));`
+      );
+    await poolConnection
+      .request()
+      .query(
+        `create table leads (id INT NOT NULL IDENTITY(1, 1), email varchar(400), type varchar(300), lead_date DATE, lead_description VARCHAR(2000), meeting_scheduled bit DEFAULT 'FALSE', meeting_date datetimeoffset(7) NULL);`
+      );
+    poolConnection.close();
+  } catch (err: any) {
+    console.error(err.message);
+  }
+}
 
 (async () => {
   client.connect(function (err: any) {
     if (err) throw err;
   });
   await client.query(
-    `CREATE TABLE IF NOT EXISTS PSS(id serial not null primary key, name varchar(500), email varchar(500), question varchar(300), answer varchar(300));`
+    `CREATE TABLE IF NOT EXISTS packaging_station(id serial not null primary key, name varchar(500), email varchar(500), question varchar(300), answer varchar(300));`
   );
   await client.query(
     `CREATE TABLE IF NOT EXISTS users(id serial not null primary key, username varchar(500), password varchar(300));`
@@ -68,18 +108,18 @@ app.post("/form-data", async (req: Request, res: Response) => {
   const email = req.body.email;
   const name = req.body.name;
   const { rows } = await client.query(
-    `SELECT * FROM PSS WHERE email = '${email}'`
+    `SELECT * FROM packaging_station WHERE email = '${email}'`
   );
   delete req.body.email;
   delete req.body.name;
   for (const key in req.body) {
     if (rows.length == 0) {
       await client.query(
-        `INSERT INTO PSS (email, name, question, answer) VALUES ('${email}','${name}','${key}','${req.body[key]}');`
+        `INSERT INTO packaging_station (email, name, question, answer) VALUES ('${email}','${name}','${key}','${req.body[key]}');`
       );
     } else {
       await client.query(
-        `UPDATE PSS SET answer='${req.body[key]}' where email = '${email}' and question='${key}';`
+        `UPDATE packaging_station SET answer='${req.body[key]}' where email = '${email}' and question='${key}';`
       );
     }
   }
@@ -88,19 +128,19 @@ app.post("/form-data", async (req: Request, res: Response) => {
 
 app.get("/form-data", verifyToken, async (req: Request, res: Response) => {
   let rows;
-  ({ rows } = await client.query(`SELECT * FROM PSS`));
+  ({ rows } = await client.query(`SELECT * FROM packaging_station`));
   if (req.query.email) {
     ({ rows } = await client.query(
-      `SELECT * FROM PSS WHERE email = '${req.query.email}'`
+      `SELECT * FROM packaging_station WHERE email = '${req.query.email}'`
     ));
   }
   if (req.query.question)
     ({ rows } = await client.query(
-      `SELECT * FROM PSS WHERE question = '${req.query.question}'`
+      `SELECT * FROM packaging_station WHERE question = '${req.query.question}'`
     ));
   if (req.query.question && req.query.answer)
     ({ rows } = await client.query(
-      `SELECT * FROM PSS WHERE question = '${req.query.question}' and answer = '${req.query.answer}'`
+      `SELECT * FROM packaging_station WHERE question = '${req.query.question}' and answer = '${req.query.answer}'`
     ));
   res.send(rows);
 });
@@ -109,15 +149,15 @@ app.post("/users", async (req: Request, res: Response) => {
   const password = req.body.password;
   const username = req.body.username;
   const { rows } = await client.query(
-    `SELECT * FROM PSS WHERE email = '${username}'`
+    `SELECT * FROM packaging_station WHERE email = '${username}'`
   );
   if (rows.length == 0) {
     await client.query(
-      `INSERT INTO PSS (username,password) VALUES ('${username}','${password}');`
+      `INSERT INTO packaging_station (username,password) VALUES ('${username}','${password}');`
     );
   } else {
     await client.query(
-      `UPDATE PSS SET password='${password}' where username = '${username}';`
+      `UPDATE packaging_station SET password='${password}' where username = '${username}';`
     );
   }
   res.send("success");
